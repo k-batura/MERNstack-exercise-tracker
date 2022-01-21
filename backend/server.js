34bb1a1915ce05aebd8e204a26fffac0b833c576
Kartikeya Batura <33941035+k-batura@users.noreply.github.com>
2022-01-20 23:34:31 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+//const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// const connection = mongoose.connection;
-// connection.once("open", () => {
-//   console.log("MongoDB database connection established successfully");
-// });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
