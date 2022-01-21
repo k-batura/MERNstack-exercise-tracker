97718f96c45db74178c8f77e8f8a76b5823400f6
Kartikeya Batura <33941035+k-batura@users.noreply.github.com>
2022-01-20 23:35:59 -0500

    testing

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI;
 
-mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
